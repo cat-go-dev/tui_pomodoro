ad1c99d487abb73a56d3ed400df05631118f5b22
Stas Amosov <amosov-stas@mail.ru>
2025-11-09 08:30:31 +0300

    fix tests

--- internal/timer/timer_test.go
+++ internal/timer/timer_test.go
@@ -1,60 +1,59 @@
 package timer
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewTimer(t *testing.T) {
+func Test_Start_Success(t *testing.T) {
 	t.Parallel()
 
-	createdTimer := NewTimer()
-	excpectedTimer := &timerImpl{}
-	assert.Equal(t, excpectedTimer, createdTimer)
+	ctx := context.Background()
+	timer := NewTimer()
+	startTime := time.Now()
+	expectedDiff := time.Duration(startTime.Unix()) + time.Millisecond*500
+	timerDur := 2 * time.Second
+
+	timer.Start(ctx, timerDur)
+	timer.Done(ctx)
+
+	finishTime := time.Now()
+
+	assert.WithinDuration(t, startTime, finishTime, expectedDiff)
 }
 
-// todo: переписать тесты
-// func Test_Start_Success(t *testing.T) {
-// 	t.Parallel()
-//
-// 	ctx := context.Background()
-// 	timer := NewTimer()
-// 	startTime := time.Now()
-// 	expectedDiff := time.Duration(startTime.Unix()) + time.Millisecond*500
-// 	timerDur := 2 * time.Second
-//
-// 	tCh := timer.Start(ctx, timerDur)
-// 	<-tCh
-//
-// 	finishTime := time.Now()
-//
-// 	assert.WithinDuration(t, startTime, finishTime, expectedDiff)
-// }
-//
-// func Test_Start_ContextDone(t *testing.T) {
-// 	t.Parallel()
-//
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	timer := NewTimer()
-// 	dur := 20 * time.Second
-// 	cancelDur := 2 * time.Second
-// 	startTime := time.Now()
-// 	expectedDiff := cancelDur + time.Millisecond*500
-//
-// 	tCh := timer.Start(ctx, dur)
-//
-// 	go func() {
-// 		time.Sleep(cancelDur)
-// 		cancel()
-// 	}()
-//
-// 	select {
-// 	case <-tCh:
-// 		assert.Fail(t, "error with context done")
-// 	case <-ctx.Done():
-// 		<-tCh
-// 		finishTime := time.Now()
-// 		assert.WithinDuration(t, startTime, finishTime, expectedDiff)
-// 	}
-// }
+func Test_Start_ContextDone(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := NewTimer()
+	dur := 20 * time.Second
+	cancelDur := 2 * time.Second
+	startTime := time.Now()
+	expectedDiff := cancelDur + time.Millisecond*500
+
+	timer.Start(ctx, dur)
+
+	go func() {
+		time.Sleep(cancelDur)
+		cancel()
+	}()
+
+	doneCh := make(chan struct{})
+	go func() {
+		timer.Done(ctx)
+		doneCh <- struct{}{}
+	}()
+
+	select {
+	case <-doneCh:
+		assert.Fail(t, "error with context done")
+	case <-ctx.Done():
+		<-doneCh
+		finishTime := time.Now()
+		assert.WithinDuration(t, startTime, finishTime, expectedDiff)
+	}
+}
